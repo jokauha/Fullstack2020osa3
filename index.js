cf1bdb7fb21fb4443d69e1100f73881f181cf682
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-08-30 18:11:20 +0200

    3.9: puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { response } = require('express')
 const app = express()
+const cors = require('cors')
 var morgan = require('morgan')
 
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny', {
     skip: (req, res) => req.method === 'POST'
