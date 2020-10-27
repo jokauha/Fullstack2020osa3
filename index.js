f6fcb8bdf58e7bea1bbeeb06d599f44934fc7da9
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-10-27 23:00:42 +0200

    3.22: lint-konfiguraatio

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const { response } = require('express')
 const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 var morgan = require('morgan')
 
-morgan.token('body', (req, res) => {
+morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
 
@@ -14,10 +13,10 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny', {
-    skip: (req, res) => req.method === 'POST'
+    skip: (req) => req.method === 'POST'
 }))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
-    skip: (req, res) => req.method !== 'POST'
+    skip: (req) => req.method !== 'POST'
 }))
 
 app.get('/api/persons', (req, res, next) => {
@@ -38,7 +37,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
-        .then(result => {
+        .then( () => {
             res.status(204).end()
         })
         .catch(error => next(error))
@@ -46,14 +45,14 @@ app.delete('/api/persons/:id', (req, res, next) => {
 
 app.post('/api/persons', (req, res, next) => {
     const body = req.body
-
+    var e = new Error('Error')
     if (body.name === '') {
-        var e = new Error('Name missing')
+        e = new Error('Name missing')
         e.name = 'NameError'
         return next(e)
     }
     else if (body.number === '') {
-        var e = new Error('Number missing')
+        e = new Error('Number missing')
         e.name = 'NumberError'
         return next(e)
     }
@@ -62,14 +61,14 @@ app.post('/api/persons', (req, res, next) => {
             name: body.name,
             number: body.number,
         })
-    
+
         person.save()
             .then(savedPerson => {
                 res.json(savedPerson)
             })
             .catch(error => next(error))
     }
-    
+
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -107,7 +106,7 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: 'no name' })
     }
     else if (error.name === 'NumberError') {
-        return response.status(400).send({ error: 'no number'}).end()
+        return response.status(400).send({ error: 'no number' }).end()
     }
     else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -16,16 +16,16 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: String,
 })
-  
+
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 5) {
-          
+
     const person = new Person({
         name: process.argv[3],
         number: process.argv[4]
     })
-      
+
     person.save().then(response => {
         console.log('added ' + response.name + ' number ' + response.number + ' to phonebook')
         mongoose.connection.close()
